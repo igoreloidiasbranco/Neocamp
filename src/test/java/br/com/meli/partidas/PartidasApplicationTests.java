952eabb98fe5ce96efcfbd2b0342c1adfb1aa6c5
igoreloidiasbranco <igoreloi1981@gmail.com>
2025-06-30 19:52:06 -0300

    commit inicial

---
+++ src/test/java/br/com/meli/partidas/PartidasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.partidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
